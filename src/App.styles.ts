bc05e0e93b303841f3a91c73479e31d801a9bcc5
Thiago <thiagofang@gmail.com>
2022-10-25 17:50:34 -0300

    mudanças no css geral

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: #27282F;
   color: #FFF;
   min-height: 100vh;
+  background: linear-gradient(90deg, rgba(2,1,15,1) 0%, rgba(10, 47, 64, 1) 170%);
 `;
 
 export const Area = styled.div`
+  display: grid;
+  row-gap: 2rem;
   margin: auto;
   max-width: 980px;
   padding: 30px 0;
@@ -16,7 +18,7 @@ export const Header = styled.h1`
   margin: 0;
   padding: 0;
   text-align: center;
-  margin-bottom: 30px;
+  text-align: initial;
 `;
 
 export const ScreenWarning = styled.div`
@@ -34,18 +36,21 @@ export const EmptyWarning = styled.div`
 
 export const PhotoList = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   gap: 10px;
 `;
 
 export const UploadForm = styled.form`
-  background-color: #3D3F43;
+  background: linear-gradient(135deg, rgba(255, 255, 255, .1) -50%, rgba(255, 255, 255, 0) 150%);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, .01);
+  box-shadow: 0 .5rem 1rem 0 rgba(0, 0, 0, .2);
   padding: 15px;
   border-radius: 10px;
-  margin-bottom: 30px;
 
   input[type=submit] {
-    background-color: #756DF4;
+    background-color: #488991;
     border: 0;
     color: #FFF;
     padding: 8px 16px;
@@ -53,6 +58,7 @@ export const UploadForm = styled.form`
     border-radius: 10px;
     margin: 0 20px;
     cursor: pointer;
+    transition: all .3s;
 
     &:hover {
       opacity: .9;
--- src/components/PhotoItem/styles.ts
+++ src/components/PhotoItem/styles.ts
@@ -1,14 +1,28 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: #3d3f43;
+  background: linear-gradient(135deg, rgba(255, 255, 255, .1) -50%, rgba(255, 255, 255, 0) 150%);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, .01);
+  box-shadow: 0 .5rem 1rem 0 rgba(0, 0, 0, .2);
   border-radius: 10px;
   padding: 10px;
 
+  
   img {
+    position: relative;
     max-width: 100%;
     display: block;
     margin-bottom: 10px;
-    border-radius: 10px;
+    border-radius: 5px;
+    box-shadow: 0 .5rem .5rem 0 rgba(0, 0, 0, .1);
+    transition: transform .3s ease-in-out, scale .3s ease-in-out;
   }
+  
+  &:hover img {
+    transform: translateY(-.5rem);
+    scale: 1.03;
+  }
+
 `;
