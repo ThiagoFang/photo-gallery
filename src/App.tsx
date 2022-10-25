c219b54d28ea97a6d0b93726626f50b6b6bb13df
Thiago <thiagofang@gmail.com>
2022-10-25 15:04:24 -0300

    criando interação entre firebase e aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import * as C from './App.styles'
 import * as Photos from './services/photos';
 import { Photo } from './types/photo';
+import { PhotoItem } from './components/PhotoItem';
 
 const App = () => {
+  const [uploading, setUploading] = useState(false);
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState<Photo[]>([]);
 
@@ -16,12 +18,38 @@ const App = () => {
     getPhotos();
   }, []);
 
+  const handleFormSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if(file && file.size > 0) {
+      setUploading(true);
+      const result = await Photos.insert(file);
+      setUploading(false);
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+
+    };
+  };
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Galeria de Fotos</C.Header>
 
-        {/* Area de upload */}
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image" />
+          <input type="submit" value="Enviar" />
+          {uploading && "Enviando..."}
+        </C.UploadForm>
 
 
         {loading &&
@@ -40,7 +68,11 @@ const App = () => {
         {!loading && photos.length > 0 &&
           <C.PhotoList>
             {photos.map((item, index) => (
-              <div key={index}>{item.name}</div>
+              <PhotoItem 
+                key={index}
+                url={item.url}
+                name={item.name}
+              />
             ))}
           </C.PhotoList>
         }
